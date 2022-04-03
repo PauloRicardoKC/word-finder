0ba57af8ac76bc514fe45207915ddc93377c3b64
Paulo <pauloricardokc@gmail.com>
2022-04-03 19:02:20 -0300

    initial commit

---
+++ WordFinderApp.Test/ProgramTest.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+using static Program;
+
+namespace WordFinderApp.Test
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void SuccessWordFinder()
+        {
+            var result = new WordFinder(WordsDictionaryMock()).Find(WordStreamMock());
+
+            Assert.True(result.Any());
+        }
+
+        [Fact]
+        public void EmptyWordFinder()
+        {
+            var result = new WordFinder(ColorWords()).Find(WordStreamMock());
+
+            Assert.False(result.Any());
+        }
+
+        private static string[] WordsDictionaryMock()
+        {
+            return new string[] { "cold", "wind", "snow", "chill" };
+        }
+
+        private static string[] ColorWords()
+        {
+            return new string[] { "blue", "red", "yellow", "green" };
+        }
+
+        private static string[] WordStreamMock()
+        {
+            return new string[] { "abcdce", "fgwiod", "chillm", "pqnsdo", "uvdxyp", "abdghe" };
+        }
+    }
+}
---
+++ WordFinderApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+public class Program
+{
+	public static void Main()
+	{
+		var wordsDictionary = new string[] { "cold", "wind", "snow", "chill" };
+		var wordStream = new string[] { "abcdce", "fgwiod", "chillm", "pqnsdo", "uvdxyp", "abdghe" };
+		var result = new WordFinder(wordsDictionary).Find(wordStream);
+		
+		Console.WriteLine(string.Join(",", result));
+	}
+
+	public class WordFinder
+	{
+		private readonly HashSet<string> matrix;
+
+		public WordFinder(IEnumerable<string> matrix)
+		{
+			this.matrix = new HashSet<string>(matrix);
+		}
+
+		public IEnumerable<string> Find(IEnumerable<string> wordStream)
+		{
+			var searchWordsLeftRight = string.Join(string.Empty, wordStream);
+
+			var characterMatrix = wordStream
+				.Select(x => x.ToCharArray())
+				.ToArray();
+
+			var searchWordsTopDownStringBuilder = new StringBuilder();
+
+			for (var i = 0; i < characterMatrix.Length; i++)
+			{
+				for (var j = 0; j < characterMatrix[i].Length; j++)
+				{
+					searchWordsTopDownStringBuilder.Append(characterMatrix[j][i]);
+				}
+			}
+
+			var searchWordsTopDown = searchWordsTopDownStringBuilder.ToString();
+
+			var result = new HashSet<string>();
+			result.UnionWith(matrix.Where(searchTerm => searchWordsLeftRight.Contains(searchTerm)));
+			result.UnionWith(matrix.Where(searchTerm => searchWordsTopDown.Contains(searchTerm)));
+
+			return result.ToList();
+		}
+	}
+}
